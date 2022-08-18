24d1dd6547920fe86131844e7dda8a674e1e5232
GitsMuthukumarJ <45478465+GitsMuthukumarJ@users.noreply.github.com>
2022-08-18 11:44:33 +0530

    Create button_css.php

---
+++ button_css.php
@@ -0,0 +1,49 @@
+<button class="button-24" role="button">No. of Results </button>&nbsp;<button class="button-24" role="button">'.$total_data.'</button>
+
+<style>
+.button-24 {
+  background: transparent;
+    border: 1px solid #FF4742;
+    border-radius: 6px;
+    box-shadow: rgb(0 0 0 / 10%) 1px 2px 4px;
+    box-sizing: border-box;
+    color: #FF4742;
+  cursor: pointer;
+  display: inline-block;
+  font-family: nunito,roboto,proxima-nova,"proxima nova",sans-serif;
+  font-size: 16px;
+  font-weight: 800;
+  line-height: 16px;
+  min-height: 40px;
+  outline: 0;
+  padding: 12px 14px;
+  text-align: center;
+  text-rendering: geometricprecision;
+  text-transform: none;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+  vertical-align: middle;
+}
+
+.button-24:hover,
+.button-24:active {
+  background-color: initial;
+  background-position: 0 0;
+  color: #FF4742;
+}
+
+.button-24:active {
+  opacity: .5;
+}
+
+.hide_mob {
+color: #7c8f89;
+}
+
+@media only screen and (max-width: 600px) {
+	.hide_mob {
+		display: none;
+	}
+}
+</style>
